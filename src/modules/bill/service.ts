8aabd0b98a1569765fbe41e8c994b7d2ff98c6cd
Emanuel Guimarães <emanuel.gui.santana@gmail.com>
2025-09-25 19:46:44 -0300

    adicionando metodos de atualizar preço de uma conta e de pegar todas as contas de uma mesa

--- src/modules/bill/service.ts
+++ src/modules/bill/service.ts
@@ -25,6 +25,38 @@ class BillService{
         return bill
     }
 
+    async getBills(tableId : string){
+        const bills = await prisma.bill.findMany({
+            where:{tableId : Number(tableId)}
+        })
+        return bills
+    }
+
+    async addProductToBill(productId : string, billId : string){
+        const product = await prisma.product.findFirst({
+            where:{id : Number(productId)}
+        })
+        if(!product){
+            throw new Error("Produto inexistente")
+        }
+        const bill = await prisma.bill.findFirst({
+            where:{id : Number(billId)}
+        })  
+        if(!bill){
+            throw new Error("Conta inexistente")
+        }   
+        const newAmount = bill.amount + product.price;
+
+        await prisma.bill.update({
+            where:{id : Number(billId)},
+            data:{amount : newAmount}
+        })
+        return {message : "Produto adicionado com sucesso"} 
+
+
+    }
+
+
 }
 
 export {BillService}
