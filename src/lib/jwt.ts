ebd8994d2a6ccfcbc95eb3679ae25434f293ffa3
Emanuel Guimarães <emanuel.gui.santana@gmail.com>
2025-10-08 14:19:17 -0300

    configurando middleware de autenticação

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,10 +1,26 @@
 import { FastifyRequest, FastifyReply } from "fastify"
 import "@fastify/jwt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET as string
+
 
 export default async function authenticate(request: FastifyRequest, reply : FastifyReply) {
-  try {
+  const authHeader = request.headers.authorization;
+  if (!authHeader) {
+    return reply.code(401).send({ error: 'Token não fornecido' });
+  }
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return reply.code(401).send({ error: 'Token mal formatado' });
+  }
+
+   try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    (request as any).user = decoded;
     await request.jwtVerify();
-  } catch (err : unknown) {
-    return reply.code(401).send({ error: 'Token inválido ou expirado' })
+
+  } catch (err) {
+    return reply.code(401).send({ error: "Token inválido ou expirado" });
   }
 }
